897548bbb7ba3cd8bea34474eb063b13555e3ccb
Mana <managhabraei2022@gmail.com>
2023-09-17 23:39:03 +0100

    first

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,11 @@
-import { ReactDOM } from "react-dom";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1> Welcome to my world</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
